a9639f6f230922775868e9ae90190e0080ddba08
kalyaniram88 <kalyaniram88@users.noreply.github.com>
2014-04-19 19:59:54 -0700

    Update makeCachematrix.R

--- makeCachematrix.R
+++ makeCachematrix.R
@@ -1,17 +1,12 @@
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL   # initialize the stored inverse value to NULL
-    set <- function(y) {     # to set the value of the matrix
+    inv <- NULL  
+    set <- function(y) {   
         x <<- y
-        inv <<- NULL # since the matrix changed
+        inv <<- NULL 
     }
-   
     get <- function() x
-    # to set the inverse
     setinv <- function(inv_) inv <<- inv_
-    # to get the inverse
     getinv <- function() inv
-
-    # return a list of all the above functions
     list(set = set, get = get,
          setinv = setinv,
          getinv = getinv)
